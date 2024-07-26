00ef06d509fcc236d5c05fb86f607f7b3213fe94
Vikram Shanbogar <70604290+vikramshanbogar@users.noreply.github.com>
2024-07-26 11:23:13 +0530

    loggin

---
+++ Address/src/main/java/org/example/address/LoggingAdvice.java
@@ -0,0 +1,38 @@
+package org.example.address;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAdvice {
+
+    Logger log = LoggerFactory.getLogger(LoggingAdvice.class);
+
+    @Pointcut(value = "execution(* com.vikram.customers.*.*(..) )")
+    public void myPointcut() {
+
+    }
+
+    @Pointcut(value = "execution(* com.vikram.customers.*.*(..) )")
+    public void myPointcut123() {
+
+    }
+
+    @Before("myPointcut()")
+    public void beforeAdvice(JoinPoint jp) {
+        log.info("in method : {}", jp.getSignature().getName());
+    }
+
+    @After("myPointcut()")
+    public void afterAdvise(JoinPoint jp) {
+        log.info("completed method : {}, {}", jp.getSignature().getName(), jp.getTarget().getClass().toString());
+    }
+
+}
